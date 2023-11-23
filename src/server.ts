d83adb5e11f6731cc268cb0846e64551ae3a5bb0
unknown <jahirulislam41185@gmail.com>
2023-11-24 01:43:45 +0600

    eslint and prettier setup completed

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,20 @@
-import mongoose from "mongoose";
-import app from "./app"
+import mongoose from 'mongoose';
+import app from './app';
 
-const port = 5000
+const port = 5000;
 
 async function main() {
-    try {
-        await mongoose.connect('mongodb+srv://user-management:user-management-app-pass@cluster0.kri1sc7.mongodb.net/advance-express-app?retryWrites=true&w=majority');
+  try {
+    await mongoose.connect(
+      'mongodb+srv://user-management:user-management-app-pass@cluster0.kri1sc7.mongodb.net/advance-express-app?retryWrites=true&w=majority',
+    );
 
-        app.listen(port, () => {
-            console.log(`app listening on port ${port}`)
-        })
-
-    } catch (error) {
-       console.log(error);
-    }
-
+    app.listen(port, () => {
+      console.log(`app listening on port ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
-
-
 
 main();
