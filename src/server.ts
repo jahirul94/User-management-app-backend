1b05c435ee7ee8e7a64646ec61c5bbc11d54838b
unknown <jahirulislam41185@gmail.com>
2023-11-24 00:41:12 +0600

    project setup complete

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+import app from "./app"
+
+const port = 5000
+
+async function main() {
+    try {
+        await mongoose.connect('mongodb+srv://user-management:user-management-app-pass@cluster0.kri1sc7.mongodb.net/advance-express-app?retryWrites=true&w=majority');
+
+        app.listen(port, () => {
+            console.log(`app listening on port ${port}`)
+        })
+
+    } catch (error) {
+       console.log(error);
+    }
+
+}
+
+
+
+main();
